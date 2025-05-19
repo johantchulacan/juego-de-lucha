1dd0655b3c08fa53dc413789a5fbb47bac5f2d83
johantchulacan <johant.chulacan@campusucc.edu.co>
2025-05-19 02:32:23 +0000

    se actualiza los parametros en la clase main

--- juegolucha/Main.java
+++ juegolucha/Main.java
@@ -9,21 +9,21 @@ public class Main {
         // Se solicitan nombres para los personajes
        System.out.print("Nombre del Guerrero: ");
         String nombreGuerrero = scanner.nextLine();
-        Arma armaGuerrero = ElegirArmas.elegirArma(scanner);
+        Arma armaGuerrero = elegirArma(scanner);
 
         System.out.print("Nombre del Mago: ");
         String nombreMago = scanner.nextLine();
-        Arma armaMago = ElegirArmas.elegirArma(scanner);
+        Arma armaMago = elegirArma(scanner);
  // Se instancian los personajes
         Guerrero guerrero = new Guerrero(nombreGuerrero, armaGuerrero);
         Mago mago = new Mago(nombreMago, armaMago);
 
 
         // Se asignan habilidades a los personajes
-     Habilidad habilidadGuerrero = ElegirHabilidades.elegirHabilidad(scanner, guerrero.getNombre());
+     Habilidad habilidadGuerrero = elegirHabilidad(scanner, guerrero.getNombre());
     guerrero.agregarHabilidad(habilidadGuerrero);
 
-    Habilidad habilidadMago = ElegirHabilidades.elegirHabilidad(scanner, mago.getNombre());
+    Habilidad habilidadMago = elegirHabilidad(scanner, mago.getNombre());
     mago.agregarHabilidad(habilidadMago);
 
 
@@ -31,5 +31,49 @@ public class Main {
         // Se crea e inicia el juego
         JuegoLucha juego = new JuegoLucha(guerrero, mago);
         juego.iniciarPelea();
+    }
+
+    public static Arma elegirArma(Scanner scanner) {
+        while (true) {
+            System.out.println("Elige un arma:");
+            System.out.println("1. Espada de Hierro");
+            System.out.println("2. Flecha");
+            System.out.println("3. Pistola");
+            System.out.print("Opción: ");
+            int opcion = scanner.nextInt();
+            scanner.nextLine(); // limpiar buffer
+
+            if (opcion == 1) {
+                return new EspadaHierro();
+            } else if (opcion == 2) {
+                return new Flecha();
+            } else if (opcion == 3) {
+                return new Pistola();
+            } else {
+                System.out.println("Opción inválida. Intenta de nuevo.");
+            }
+        }
+    }
+
+    public static Habilidad elegirHabilidad(Scanner scanner, String nombre) {
+        while (true) {
+            System.out.println("Elige una habilidad para " + nombre + ":");
+            System.out.println("1. Saltar");
+            System.out.println("2. SuperPoder");
+            System.out.println("3. Correr Rápido");
+            System.out.print("Opción: ");
+            int opcion = scanner.nextInt();
+            scanner.nextLine(); // limpiar buffer
+
+            if (opcion == 1) {
+                return new Saltar();
+            } else if (opcion == 2) {
+                return new SuperPoder();
+            } else if (opcion == 3) {
+                return new CorrerRapido();
+            } else {
+                System.out.println("Opción inválida. Intenta de nuevo.");
+            }
+        }
     }
 }
